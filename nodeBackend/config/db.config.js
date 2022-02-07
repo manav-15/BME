1be3e1a82fd64afba9cb70f69d0b428163e12a49
Manav Chirania <chiraniamanav15@gmail.com>
2022-02-08 03:12:42 +0530

    Changed config for deplpoyment

--- nodeBackend/config/db.config.js
+++ nodeBackend/config/db.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  // username = doadmin
+  // password = BJVyF3P4ysDP5y2h
+  // HOST = "private-db-mysql-blr1-39804-do-user-10835358-0.b.db.ondigitalocean.com",
+  // port = 25060
+  // database = defaultdb
+  // sslmode = REQUIRED
     HOST: "localhost",
     USER: "root",
     PASSWORD: "123456789",
--- nodeBackend/models/index.js
+++ nodeBackend/models/index.js
@@ -1,17 +1,18 @@
 const {Sequelize} = require('sequelize');
 
 const dbConfig = require("../config/db.config.js");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const sequelize = new Sequelize('mysql://doadmin:BJVyF3P4ysDP5y2h@private-db-mysql-blr1-39804-do-user-10835358-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED');
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   }
+// });
 
 
 const db = {};
